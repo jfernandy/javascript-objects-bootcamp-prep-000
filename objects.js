ad4ff430f1d42f65d418e7f2dc733d3574620be6
Jason Fernandes <jason.fernandes17@gmail.com>
2019-06-20 21:28:46 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {eminem: "criminal"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, playlist, { [artistName]: songTitle})
+  playlist[artistName] = songTitle
 }
 
 function removeFromPlaylist(playlist, artistName, songTitle) {
